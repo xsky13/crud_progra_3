4e35651a0f193b7fbccf2063ca742e442246fd03
Jared Peter <aljapeter05@gmail.com>
2026-04-13 16:43:05 -0300

    agregue pagina de registro

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,17 +1,22 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Home from './routes/Home.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Home from "./routes/Home.tsx";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
-import React from 'react';
+import React from "react";
+import Registro from "./routes/Registro.tsx";
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		Component: Home,
-	},
+    {
+        path: "/",
+        Component: Home,
+    },
+    {
+        path: "/registro",
+        Component: Registro,
+    },
 ]);
 
-createRoot(document.getElementById('root')!).render(
-	React.createElement(RouterProvider, { router })
+createRoot(document.getElementById("root")!).render(
+    React.createElement(RouterProvider, { router }),
 );
