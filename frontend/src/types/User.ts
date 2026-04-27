c7f11cc2bf37b2d5c11af064677f88e4232199d5
Jared Peter <aljapeter05@gmail.com>
2026-04-27 10:40:32 -0300

    added user types and minor tsconfig changes

---
+++ frontend/src/types/User.ts
@@ -0,0 +1,12 @@
+enum UserRole {
+    Usuario = 1,
+    Admin = 2,
+}
+
+export type User = {
+    nombre: string;
+    apellido: string;
+    email: string;
+    contrasena: string;
+    rol: UserRole;
+};
